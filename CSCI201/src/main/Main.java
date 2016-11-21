4cfc8f0ae9e01a2699f64d09a89b945a5547a6be
Janson Lau <jansonla@usc.edu>
2016-11-21 01:07:16 -0800

    initial commit

---
+++ CSCI201/src/main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import frames.LoginGUI;
+//import frames.LoginGUIJFX;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		new LoginGUI().setVisible(true);
+	}
+}
